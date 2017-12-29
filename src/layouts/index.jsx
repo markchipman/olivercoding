e3b15ac4b365e17355b3fa0e720ea666173d05d7
Daniel Oliver <11494375+DanielOliver@users.noreply.github.com>
2017-12-29 14:47:39 -0500

    Refactoring to navigation bar.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,47 +3,12 @@ import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import Link from "gatsby-link";
 import Footer from "../components/Footer/Footer";
+import NavBar from "../components/NavBar/NavBar";
+import Header from "../components/Header/Header";
+
 
 import logo from "../static/logo.svg";
 import "./index.scss";
-
-const Header = () => (
-  <section className="hero is-primary">
-    <div className="hero-body">
-      <div className="container">
-          <div className="columns is-mobile is-centered is-is-multiline">
-            <div className="column is-narrow">
-              <Link
-                  to="/"
-                  style={{
-                    color: "rgb(201, 182, 76)",
-                    textDecoration: "none",
-                    textShadow: "1px 1px #111122",
-                    margin: 0
-                  }}
-                >
-                <img className="image is-128x128"
-                  src={logo}/>
-              </Link>
-            </div>
-          <div className="column">
-              <Link
-                  to="/"
-                  style={{
-                    color: "rgb(201, 182, 76)",
-                    textDecoration: "none",
-                    textShadow: "1px 1px #111122",
-                    margin: 0
-                  }}
-                >
-                <h1 className="title">OLIVER<br/>CODING</h1>
-              </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-);
 
 export default class MainLayout extends React.Component {
   getLocalTitle() {
@@ -88,7 +53,8 @@ export default class MainLayout extends React.Component {
           <title>{`${config.siteTitle} |  ${this.getLocalTitle()}`}</title>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
-        <Header />
+        <NavBar logo={logo} />
+        <Header logo={logo} />
         <div className="container">
           {children()}
         </div>
